1f171fcee2a32f443db43cf899b6b378dd1e4444
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-27 04:11:24 -0400

    #1: only build readline extension if header exists

--- ext/readline_line_buffer/extconf.rb
+++ ext/readline_line_buffer/extconf.rb
@@ -1,3 +1,4 @@
 require "mkmf"
+have_header 'readline/readline.h'
 dir_config 'readline_line_buffer'
 create_makefile 'readline_line_buffer'
--- ext/readline_line_buffer/readline_line_buffer.c
+++ ext/readline_line_buffer/readline_line_buffer.c
@@ -2,6 +2,7 @@
    Copyright (C) 1997-2001  Shugo Maeda */
 /* body of line_buffer() from irb enhancements at http://www.creo.hu/~csaba/ruby/ */
  
+#ifdef HAVE_READLINE_READLINE_H
 #include "ruby.h"
 #include <errno.h>
 #include <stdio.h>
@@ -20,3 +21,4 @@ void Init_readline_line_buffer() {
   c = rb_const_get(c, rb_intern("Readline"));
   rb_define_singleton_method(c, "line_buffer", (VALUE(*)(ANYARGS))line_buffer, -1);
 }
+#endif
