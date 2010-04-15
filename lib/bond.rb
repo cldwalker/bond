4c228f20cd8edab79d1d75738630e2e1a26051bb
Gabriel Horner <gabriel.horner@gmail.com>
2010-04-15 05:58:30 -0400

    got rid of $LOAD_PATH manipulation

--- lib/bond.rb
+++ lib/bond.rb
@@ -1,5 +1,3 @@
-current_dir = File.dirname(__FILE__)
-$:.unshift(current_dir) unless $:.include?(current_dir) || $:.include?(File.expand_path(current_dir))
 require 'bond/readline'
 require 'bond/rawline'
 require 'bond/agent'
