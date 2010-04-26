757fe78ca9c08ab21a5bf0f00cf30dc7b9162bf2
Gabriel Horner <gabriel.horner@gmail.com>
2010-04-25 20:01:32 -0400

    added names to completions likely to be replaced

--- lib/bond/completion.rb
+++ lib/bond/completion.rb
@@ -4,7 +4,7 @@ complete :object=>"Object"
 complete :all_methods=>true
 complete :all_operator_methods=>true
 # classes and constants
-complete(:anywhere=>'(((::)?[A-Z][^:.\(]*)+)::?([^:.]*)') {|e|
+complete(:name=>:constants, :anywhere=>'(((::)?[A-Z][^:.\(]*)+)::?([^:.]*)') {|e|
   receiver = e.matched[2]
   candidates = Mission.current_eval("#{receiver}.constants | #{receiver}.methods")
   default_search(e.matched[5], candidates).map {|e| receiver + "::" + e}
@@ -13,4 +13,4 @@ complete(:anywhere=>'(((::)?[A-Z][^:.\(]*)+)::?([^:.]*)') {|e|
 complete(:prefix=>'::', :anywhere=>'[A-Z][^:\.\(]*') {|e| Object.constants }
 complete(:anywhere=>':[^:\s.]*') {|e|  Symbol.all_symbols.map {|f| ":#{f}" } rescue [] }
 complete(:anywhere=>'\$[^\s.]*') {|e| global_variables }
-complete(:on=>/[\s(]["']([^'"]*)$/, :search=>false, :place=>:last) {|e| files(e.matched[1]) }
+complete(:name=>:quoted_files, :on=>/[\s(]["']([^'"]*)$/, :search=>false, :place=>:last) {|e| files(e.matched[1]) }
