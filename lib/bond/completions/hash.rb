4da126b378be26702e5cc8aa5bb01a825561b820
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-07 05:38:05 -0400

    fixed hash method completion

--- lib/bond/completions/hash.rb
+++ lib/bond/completions/hash.rb
@@ -1,3 +1,3 @@
 complete(:methods=>%w{delete fetch store, [] has_key? key? include? member? values_at},
   :class=>"Hash#") {|e| e.object.keys }
-complete(:method=>%w{index value? has_value?}, :class=>"Hash#") {|e| e.object.values }
+complete(:methods=>%w{index value? has_value?}, :class=>"Hash#") {|e| e.object.values }
