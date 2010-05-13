0b55c0a33ba49aba3fbbd5bf6bc94182f0bdc0bc
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-13 18:55:37 -0400

    updating constants regex using 1.9.2 changes

--- lib/bond/completion.rb
+++ lib/bond/completion.rb
@@ -4,10 +4,10 @@ complete :object=>"Object"
 complete :all_methods=>true
 complete :all_operator_methods=>true
 # classes and constants
-complete(:name=>:constants, :anywhere=>'(((::)?[A-Z][^:. \(]*)+)::?([^: .]*)') {|e|
+complete(:name=>:constants, :anywhere=>'([A-Z][^:. \(]*)::([^: .]*)') {|e|
   receiver = e.matched[2]
   candidates = eval("#{receiver}.constants | #{receiver}.methods") || []
-  normal_search(e.matched[5], candidates).map {|e| receiver + "::" + e}
+  normal_search(e.matched[3], candidates).map {|e| receiver + "::" + e}
 }
 # absolute constants
 complete(:prefix=>'::', :anywhere=>'[A-Z][^:\.\(]*') {|e| Object.constants }
