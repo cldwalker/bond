c647f52f1b57b034af87fa08ff524eb20b3281d5
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-13 05:35:23 -0400

    tweak public, private + protected Module methods

--- lib/bond/completions/module.rb
+++ lib/bond/completions/module.rb
@@ -1,7 +1,10 @@
 complete(:methods=>%w{const_get const_set const_defined? remove_const}, :class=>"Module#") {|e| e.object.constants }
 complete(:methods=>%w{class_variable_defined? class_variable_get class_variable_set remove_class_variable},
   :class=>"Module#") {|e| e.object.class_variables }
-complete(:methods=>%w{instance_method method_defined? module_function public private protected remove_method undef_method},
+complete(:methods=>%w{instance_method method_defined? module_function remove_method undef_method},
   :class=>"Module#") {|e| e.object.instance_methods }
+complete(:method=>"Module#public") {|e| e.object.private_instance_methods + e.object.protected_instance_methods }
+complete(:method=>"Module#private") {|e| e.object.instance_methods + e.object.protected_instance_methods }
+complete(:method=>"Module#protected") {|e| e.object.instance_methods + e.object.private_instance_methods }
 complete(:methods=>%w{< <= <=> > >= include? include}, :class=>"Module#", :search=>:modules) { objects_of(Module) }
 complete(:method=>'Module#alias_method') {|e| e.argument > 1 ? e.object.instance_methods : [] }
