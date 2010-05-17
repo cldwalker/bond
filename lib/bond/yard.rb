1147de8b8ddfb597648a0080008d9873054a5b54
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-17 00:56:44 -0400

    option to reload yardocs, tweaked error handling

--- lib/bond/yard.rb
+++ lib/bond/yard.rb
@@ -1,38 +1,36 @@
 module Bond
-  class YardError < StandardError; end
   module Yard
     extend self
 
     def load_yard_gems(*gems)
-      require_yard
+      @options = gems[-1].is_a?(Hash) ? gems.pop : {}
+      require 'yard'
       gems.each {|e| load_yard_gem(e) }
-    rescue YardError
-      $stderr.puts "Bond Error: #{$!.message}"
+    rescue LoadError
+      $stderr.puts "Bond Error: yard gem not installed"
     end
 
     def load_yard_gem(rubygem)
-      if (yardoc = find_yardoc(rubygem))
-        completion_file = File.join(dir('yard_completions'), rubygem+'.rb')
-        unless File.exists? completion_file
-          YARD::Registry.load!(yardoc)
-          methods_hash = find_methods_with_options
-          body = generate_method_completions(methods_hash)
-          File.open(completion_file, 'w') {|e| e.write body }
-        end
-        M.load_file completion_file
-      else
-        raise YardError, "Failed to load yard gem '#{rubygem}'. Unable to find its .yardoc."
+      raise("Unable to find gem.") unless (yardoc = find_yardoc(rubygem))
+      completion_file = File.join(dir('yard_completions'), rubygem+'.rb')
+      if !File.exists?(completion_file) || @options[:reload]
+        YARD::Registry.load!(yardoc)
+        methods_hash = find_methods_with_options
+        body = generate_method_completions(methods_hash)
+        File.open(completion_file, 'w') {|e| e.write body }
       end
-    rescue YardError
-      $stderr.puts "Bond Error: #{$!.message}"
+      M.load_file completion_file
+    rescue
+      $stderr.puts "Bond Error: Didn't load yard completions for gem '#{rubygem}'. #{$!.message}"
     end
 
     def find_yardoc(rubygem)
       (file = YARD::Registry.yardoc_file_for_gem(rubygem)) and return(file)
       if (file = M.find_gem_file(rubygem, rubygem+'.rb'))
         output_dir = File.join(dir('.yardocs'), rubygem)
-        cmd = ['yardoc', '-n', '-c', output_dir, '-b', output_dir, file,
-          File.expand_path(file+'/..')+"/#{rubygem}/**/*.rb"]
+        cmd = ['yardoc', '-n', '-b', output_dir]
+        cmd += ['-c', output_dir] unless @options[:reload]
+        cmd += [file, File.expand_path(file+'/..')+"/#{rubygem}/**/*.rb"]
         puts cmd.join(' '), "Generating #{rubygem}'s YARD documentation ..."
         system *cmd
         output_dir
@@ -58,12 +56,6 @@ module Bond
         options.map! {|e| e.sub(/^:/, '') }
         %Q[complete(:method=>'#{meth}') {\n  #{options.inspect}\n}]
       end.join("\n")
-    end
-
-    def require_yard
-      require 'yard'
-    rescue LoadError
-      raise YardError, "yard gem not installed"
     end
   end
 end
