5f64e694b83c9afddb8427b2439c6de8d97a4d20
Gabriel Horner <gabriel.horner@gmail.com>
2010-07-07 03:37:55 -0400

    Tweaks to tim's patch. Closes gh-3

--- ext/readline_line_buffer/extconf.rb
+++ ext/readline_line_buffer/extconf.rb
@@ -2,12 +2,12 @@ require "mkmf"
 
 if RUBY_VERSION < '1.9.2'
   dir_config("readline")
-  dir_config 'readline_line_buffer'
   unless have_header('readline/readline.h')
-    STDERR.puts "-" * 80
-    STDERR.puts "Error! Cannot find readline/readline.h"
-    STDERR.puts "If you have it installed in a non-standard directory, try installing the gem via `gem install bond -- --with-readline-dir=/path/to/readline`"
-    STDERR.puts "-" * 80
+    $stderr.puts "-" * 80
+    $stderr.puts "Error! Cannot find readline/readline.h."
+    $stderr.puts "Readline was probably installed in a non-standard directory.",
+      "Try `gem install bond -- --with-readline-dir=/path/to/readline`."
+    $stderr.puts "-" * 80
     exit 1
   end
   create_makefile 'readline_line_buffer'
