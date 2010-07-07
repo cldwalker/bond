2ae8474337c810ef504b4461a5c392707dd08255
Tim Harper <timcharper@gmail.com>
2010-07-06 20:33:38 -0600

    fixes ext building for non-standard Readline installs
    * extconf.rb accepts --with-readline-dir configuration param
    * it fails if readline/readline.h not found
    
    Why is this needed?  Homebrew does not link Readline by default into the
    homebrew prefix, as a precaution to prevent conflicts. (and, if my
    memory serves me, I have run into conflicts by so linking)

--- ext/readline_line_buffer/extconf.rb
+++ ext/readline_line_buffer/extconf.rb
@@ -1,4 +1,11 @@
 require "mkmf"
-have_header 'readline/readline.h'
+dir_config("readline")
 dir_config 'readline_line_buffer'
+unless have_header('readline/readline.h')
+  STDERR.puts "-" * 80
+  STDERR.puts "Error! Cannot find readline/readline.h"
+  STDERR.puts "If you have it installed in a non-standard directory, try installing the gem via `gem install bond -- --with-readline-dir=/path/to/readline`"
+  STDERR.puts "-" * 80
+  exit 1
+end
 create_makefile 'readline_line_buffer'
