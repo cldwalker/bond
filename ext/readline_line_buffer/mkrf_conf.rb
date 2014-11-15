93e344f18d0452b93630b8ffa0af89ceaddfad14
Kai Dührkop <git@k-du.de>
2014-11-15 16:03:15 +0100

    switch from make to rake

---
+++ ext/readline_line_buffer/mkrf_conf.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'rubygems/command.rb'
+require 'rubygems/dependency_installer.rb'
+begin
+  Gem::Command.build_args = ARGV
+  rescue NoMethodError
+end
+
+if Gem::Command.build_args.include?("--without-readline") || RUBY_PLATFORM =~ /java/
+  # create dummy rakefile to indicate success
+  puts "create dummy RAKE file!!!"
+  puts %Q[#{File.join(File.dirname(__FILE__), "Rakefile")}]
+  File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w") do |io|
+    io.write("task :default\n")
+  end
+else
+  puts "create boring makefile"
+  require "mkmf"
+  dir_config("readline")
+  have_library('readline')
+
+  if !have_header('readline/readline.h')
+    abort "\n** Bond Install Error: Unable to find readline.h. Please try again. **\n"+
+    "To install with your readline: gem install bond -- --with-readline-dir=/path/to/readline\n"+
+    "To install without readline: gem install bond -- --without-readline"
+  else
+    create_makefile 'readline_line_buffer'
+  end
+  File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w") do |io|
+    io.puts <<RAKE
+task :default do
+  sh "make"
+  sh "make install"
+end
+RAKE
+  end
+end
