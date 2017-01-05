ede095aec00e45e7e2aa3e89ca33150289f34b4d
Kouhei Sutou <kou@clear-code.com>
2017-01-06 00:00:29 +0900

    Remove needless extension
    All Rubies that don't support Readline.line_buffer are EOL.

--- lib/bond/readline.rb
+++ lib/bond/readline.rb
@@ -18,17 +18,10 @@ class Bond::Readline
 
   def self.readline_setup
     require 'readline'
-    load_extension unless Readline.respond_to?(:line_buffer)
     if (Readline::VERSION rescue nil).to_s[/editline/i]
       puts "Bond has detected EditLine and may not work with it." +
         " See the README's Limitations section."
     end
-  end
-
-  def self.load_extension
-    require 'readline_line_buffer'
-  rescue LoadError
-    $stderr.puts "Bond Error: Failed to load readline_line_buffer. Ensure that it exists and was built correctly."
   end
 
   # Returns full line of what the user has typed.
